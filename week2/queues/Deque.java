3ff4fcae35a31874dce79568f649a77df5e0a51a
antonc27 sw <antonc27@mail.ru>
2019-10-26 19:59:39 +0200

    Dummy Deque impl

---
+++ week2/queues/Deque.java
@@ -0,0 +1,54 @@
+/* *****************************************************************************
+ *  Name: Deque
+ *  Date: 26/10/19
+ *  Description: Generalization of a stack and a queue that supports adding and removing items from either the front or the back
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    // construct an empty deque
+    public Deque() {
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return -1;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        return null;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        return null;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+}
