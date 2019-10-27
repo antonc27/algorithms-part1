637bbb718668fd43a8314d095860dc1db85337f8
antonc27 sw <antonc27@mail.ru>
2019-10-27 20:52:02 +0100

    Dummy BruteCollinearPoints

---
+++ week3/collinear/BruteCollinearPoints.java
@@ -0,0 +1,55 @@
+/* *****************************************************************************
+ *  Name: BruteCollinearPoints
+ *  Date: 27/10/19
+ *  Description: Examines 4 points at a time and checks whether they all lie on the same line segment, returning all such line segments.
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return new LineSegment[0];
+    }
+
+    public static void main(String[] args) {
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+}
