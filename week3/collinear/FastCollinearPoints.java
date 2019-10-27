842f789e4ec9eeead2477412e57e9bf32efbde06
antonc27 sw <antonc27@mail.ru>
2019-10-27 21:50:32 +0100

    Dummy FastCollinearPoints

---
+++ week3/collinear/FastCollinearPoints.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name: FastCollinearPoints
+ *  Date: 27/10/19
+ *  Description: Finds colllinear points with n^2 * log(n) time in the worst case
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return new LineSegment[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
