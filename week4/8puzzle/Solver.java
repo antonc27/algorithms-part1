ed6f4c6e97f6fd1daf4a73a0b4e04cdd7defadad
antonc27 sw <antonc27@mail.ru>
2019-11-03 17:44:26 +0100

    Dummy Board and Solver classes

---
+++ week4/8puzzle/Solver.java
@@ -0,0 +1,34 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return false;
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+        return 0;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+    }
+
+}
