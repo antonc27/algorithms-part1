18ea96323b4cb1fcd65c28f1ce4249d4a98521cf
antonc27 sw <antonc27@mail.ru>
2019-11-03 22:48:18 +0100

    Solver for solvable puzzles

--- week4/8puzzle/Solver.java
+++ week4/8puzzle/Solver.java
@@ -5,28 +5,84 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Comparator;
 
 public class Solver {
 
+    private class SearchNode {
+        private final SearchNode parent;
+        private final Board board;
+        private final int moves;
+
+        public SearchNode(SearchNode parent, Board board, int moves) {
+            this.parent = parent;
+            this.board = board;
+            this.moves = moves;
+        }
+
+        public int priority() {
+            return board.manhattan() + moves;
+        }
+    }
+
+    private SearchNode goalNode = null;
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        if (initial == null) {
+            throw new IllegalArgumentException("Null initial board");
+        }
 
+        Comparator<Integer> natural = Comparator.naturalOrder();
+        MinPQ<SearchNode> queue = new MinPQ<>((o1, o2) -> natural.compare(o1.priority(), o2.priority()));
+
+        SearchNode root = new SearchNode(null, initial, 0);
+        queue.insert(root);
+
+        while (!queue.isEmpty()) {
+
+            SearchNode minNode = queue.delMin();
+            Board board = minNode.board;
+
+            if (board.isGoal()) {
+                goalNode = minNode;
+                break;
+            } else {
+                Board parent = (minNode.parent == null) ? null : minNode.parent.board;
+                for (Board next : board.neighbors()) {
+                    // critical optimisation
+                    if (!next.equals(parent)) {
+                        SearchNode nextNode = new SearchNode(minNode, next, minNode.moves + 1);
+                        queue.insert(nextNode);
+                    }
+                }
+            }
+        }
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        return false;
+        return goalNode != null;
     }
 
     // min number of moves to solve initial board
     public int moves() {
-        return 0;
+        return goalNode.moves;
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        return null;
+        Stack<Board> boards = new Stack<>();
+        SearchNode current = goalNode;
+        while (current != null) {
+            boards.push(current.board);
+            current = current.parent;
+        }
+        return boards;
     }
 
     // test client (see below)
