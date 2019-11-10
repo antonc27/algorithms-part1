116c676c650377c496d47b565d2a2c8a34191f86
antonc27 sw <antonc27@mail.ru>
2019-11-11 00:46:49 +0100

    Dummy PointSET and KdTree

---
+++ week5/kdtree/KdTree.java
@@ -0,0 +1,57 @@
+/* *****************************************************************************
+ *  Name: KdTree
+ *  Date: 11/11/19
+ *  Description: Represents a set of points in the unit square, supports efficient implementation of range search and nearest-neighbor search
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.ArrayList;
+
+public class KdTree {
+    // construct an empty set of points
+    public KdTree() {
+
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        return new ArrayList<>();
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        return new Point2D(0, 0);
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
+}
