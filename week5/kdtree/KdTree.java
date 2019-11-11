5526e117da1e2f4c9ae80bbc5810903387bd642b
antonc27 sw <antonc27@mail.ru>
2019-11-11 15:14:12 +0100

    KdTree: insert, contains and draw

--- week5/kdtree/KdTree.java
+++ week5/kdtree/KdTree.java
@@ -6,10 +6,38 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.ArrayList;
 
 public class KdTree {
+
+    private enum NodeType {
+        HORIZ, VERT
+    }
+
+    private enum Direction {
+        UNKNOWN, LEFT, RIGHT
+    }
+
+    private class Node {
+        Point2D point;
+        NodeType type;
+        RectHV rect;
+
+        Node left = null;
+        Node right = null;
+
+        Node(Point2D point, NodeType type, RectHV rect) {
+            this.point = point;
+            this.type = type;
+            this.rect = rect;
+        }
+    }
+
+    private Node root = null;
+    private int size = 0;
+
     // construct an empty set of points
     public KdTree() {
 
@@ -17,27 +45,153 @@ public class KdTree {
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return (root == null);
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        if (contains(p)) return;
 
+        if (root == null) {
+            RectHV whole = new RectHV(0, 0, 1, 1);
+            root = new Node(p, NodeType.VERT, whole);
+        } else {
+            insertRec(null, Direction.UNKNOWN, root, p);
+        }
+
+        size++;
+    }
+
+    private void insertRec(Node parent, Direction direction, Node current, Point2D p) {
+        if (current == null) {
+            RectHV newRect = splitRect(parent.rect, parent.point, parent.type, direction);
+            NodeType newType = (parent.type == NodeType.HORIZ) ? NodeType.VERT : NodeType.HORIZ;
+
+            Node newNode = new Node(p, newType, newRect);
+
+            switch (direction) {
+                case LEFT:
+                    assert parent.left == null;
+                    parent.left = newNode;
+                    break;
+                case RIGHT:
+                    assert parent.right == null;
+                    parent.right = newNode;
+                    break;
+                default:
+                    throw new RuntimeException("UNKNOWN case on rec insert");
+            }
+        } else {
+            Point2D currP = current.point;
+            switch (current.type) {
+                case VERT:
+                    if (p.x() < currP.x()) {
+                        insertRec(current, Direction.LEFT, current.left, p);
+                    } else {
+                        insertRec(current, Direction.RIGHT, current.right, p);
+                    }
+                    break;
+                case HORIZ:
+                    if (p.y() < currP.y()) {
+                        insertRec(current, Direction.LEFT, current.left, p);
+                    } else {
+                        insertRec(current, Direction.RIGHT, current.right, p);
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("Bad node type on rec insert");
+            }
+        }
+    }
+
+    private RectHV splitRect(RectHV toSplit, Point2D p, NodeType type, Direction direction) {
+        switch (type) {
+            case VERT:
+                if (direction == Direction.LEFT) {
+                    return new RectHV(toSplit.xmin(), toSplit.ymin(), p.x(), toSplit.ymax());
+                } else if (direction == Direction.RIGHT) {
+                    return new RectHV(p.x(), toSplit.ymin(), toSplit.xmax(), toSplit.ymax());
+                }
+                break;
+            case HORIZ:
+                if (direction == Direction.LEFT) {
+                    return new RectHV(toSplit.xmin(), toSplit.ymin(), toSplit.xmax(), p.y());
+                } else if (direction == Direction.RIGHT) {
+                    return new RectHV(toSplit.xmin(), p.y(), toSplit.xmax(), toSplit.ymax());
+                }
+                break;
+        }
+        throw new RuntimeException("Unsplittable rect");
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        return findRec(root, p) != null;
+    }
+
+    private Point2D findRec(Node current, Point2D p) {
+        if (current == null) {
+            return null;
+        } else {
+            Point2D currP = current.point;
+            if (currP.equals(p)) return currP;
+
+            switch (current.type) {
+                case VERT:
+                    if (p.x() < currP.x()) {
+                        return findRec(current.left, p);
+                    } else {
+                        return findRec(current.right, p);
+                    }
+                case HORIZ:
+                    if (p.y() < currP.y()) {
+                        return findRec(current.left, p);
+                    } else {
+                        return findRec(current.right, p);
+                    }
+                default:
+                    throw new RuntimeException("Bad node type on rec find");
+            }
+        }
     }
 
     // draw all points to standard draw
     public void draw() {
+        drawRec(root);
+    }
 
+    private void drawRec(Node current) {
+        if (current == null) return;
+
+        drawRec(current.left);
+        drawNode(current);
+        drawRec(current.right);
+    }
+
+    private void drawNode(Node current) {
+        RectHV rect = current.rect;
+        Point2D p = current.point;
+
+        StdDraw.setPenRadius();
+        switch (current.type) {
+            case VERT:
+                StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.line(p.x(), rect.ymin(), p.x(), rect.ymax());
+                break;
+            case HORIZ:
+                StdDraw.setPenColor(StdDraw.BLUE);
+                StdDraw.line(rect.xmin(), p.y(), rect.xmax(), p.y());
+                break;
+        }
+
+        StdDraw.setPenRadius(0.01);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        p.draw();
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -50,8 +204,31 @@ public class KdTree {
         return new Point2D(0, 0);
     }
 
+    private static void assertTrue(boolean cond, String msg) {
+        if (!cond) {
+            throw new RuntimeException(msg);
+        }
+    }
+
     // unit testing of the methods (optional)
     public static void main(String[] args) {
+        KdTree set = new KdTree();
 
+        assertTrue(set.isEmpty(), "emptyness");
+        assertTrue(set.size() == 0, "empty size");
+
+        Point2D p = new Point2D(0, 0);
+        assertTrue(!set.contains(p), "contains before an insert");
+        set.insert(p);
+        assertTrue(set.contains(p), "contains after an insert");
+
+        assertTrue(!set.isEmpty(), "non empty after an insert");
+        assertTrue(set.size() == 1, "size after an insert");
+
+        Point2D p1 = new Point2D(1, 1);
+        set.insert(p1);
+
+        assertTrue(set.size() == 2, "size after an another insert");
+        assertTrue(set.contains(p1), "contains after an another insert");
     }
 }
